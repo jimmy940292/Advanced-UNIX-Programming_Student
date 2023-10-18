35370b837e61ecf3d65b1c9a3df20538cf998d5b
jimmy940292 <jimmy30213@gmail.com>
2023-10-18 19:21:37 +0800

    finish assignment3

--- assignment3/assignment3.c
+++ assignment3/assignment3.c
@@ -15,18 +15,19 @@ struct CustomStream
 
 int read_from_memory(void *cookie, char *buf, int size)
 {
-    printf("Read\n");
-    printf("buf: %s\n", buf);
-    printf("size: %d\n", size);
-    int tmp = fread(&buf, 1, size, cookie);
-    printf("c: %c\n", fgetc(cookie));
-    printf("tmp: %d\n", tmp);
-    return fread(&buf, 1, size, cookie);
+    // printf("Read\n");
+    // printf("buf: %s\n", buf);
+    // printf("size: %d\n", size);
+    // printf("cookie: %s\n", cookie);
+    // int tmp = fread(&buf, 1, 12, cookie);
+    // printf("c: %c\n", fgetc(cookie));
+    // printf("tmp: %d\n", tmp);
+    return fread(buf, 1, size, cookie);
 }
 
 int write_to_memory(void *cookie, const char *buf, int size)
 {
-    printf("Write\n"); // for checking
+    // printf("Write\n"); // for checking
     // printf("%s\n", buf); // for checking
     // printf("%d\n", size); // for checking
     return fwrite(buf, 1, size, cookie);
@@ -34,8 +35,8 @@ int write_to_memory(void *cookie, const char *buf, int size)
 
 int seek_memory(void *cookie, long offset, int whence)
 {
-    printf("Seek\n");
-    printf("%ld\n", offset);
+    // printf("Seek\n");
+    // printf("%ld\n", offset);
     FILE *memfile = (FILE *)cookie;
     return fseek(memfile, (long)offset, whence);
 }
@@ -43,7 +44,7 @@ int seek_memory(void *cookie, long offset, int whence)
 int close_memory(void *cookie)
 {
     // Implement closing your memory buffer here.
-    printf("Close\n");
+    // printf("Close\n");
     fclose(cookie); // close the file
     return 0;
 }
@@ -52,19 +53,30 @@ int main()
 {
     size_t numwritten;
     char input_data[12] = "hello, world"; // len = 12
+    char new_buffer[1024];
 
-    FILE *file = fopen("output.txt", "w");
+    FILE *file = fmemopen(new_buffer, 1024, "rw");
     FILE *customStream = funopen(file, read_from_memory, write_to_memory, seek_memory, close_memory);
 
-    size_t bytesWrite = fwrite(input_data, 1, 12, customStream);
-    printf("bytesWrite: %zu\n", bytesWrite);
+    size_t bytesWrite = write_to_memory(file, input_data , 12);
+    // printf("bytesWrite: %zu\n", bytesWrite);
 
-    int seekPos = fseek(customStream, 0, SEEK_SET);
-    printf("seekPos: %d\n", seekPos);
+    char buffer[1024] = "";
 
-    char buffer[1024];
-    size_t bytesRead = fread(buffer, 1, 12, customStream);
-    printf("%zu\n", bytesRead);
+    // Print "world"
+    int seekPos = seek_memory(file, 7, SEEK_SET);
+    // printf("seekPos: %d\n", seekPos);
+
+    size_t bytesRead = read_from_memory(file, buffer, 5);
+    // printf("%zu\n", bytesRead);
+    printf("%s\n", buffer);
+
+    // Print "hello, world"
+    seekPos = seek_memory(file, 0, SEEK_SET);
+    // printf("seekPos: %d\n", seekPos);
+
+    bytesRead = read_from_memory(file, buffer, 12);
+    // printf("%zu\n", bytesRead);
     printf("%s\n", buffer);
 
     fclose(customStream); // trigger the close_to_memory function
