3adf0349db03e7a621d7987e1b437cebea6a92bd
jimmy940292 <jimmy30213@gmail.com>
2023-10-24 14:04:36 +0800

    finish mock test

--- mock_test/mock_test.c
+++ mock_test/mock_test.c
@@ -6,73 +6,155 @@
 #include <string.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
-{
-    char list[100][100];
 
-    int params_c = 0;
-    int params_i = 0;
-    int params_u = 0;
+int params_c = 0;
+int params_i = 0;
+int params_u = 0;
+int option_all = 0;
+int tot_index = 0;
+int opt;
 
-    // Read parameters
-    for(int i = 1;i < argc;i++)
-    {
-        if (strcmp(argv[i], "-c") == 0)
-        {
-            params_c = 1;
-        }
-        else if (strcmp(argv[i], "-i") == 0)
-        {
-            params_i = 1;
-        }
-        else if (strcmp(argv[i], "-u") == 0)
-        {
-            params_u = 1;
-        }
-        
+int compare_func(char *s1, char *s2){
+    
+    if(params_i == 1){
+        return strcasecmp(s1, s2);
     }
-    printf("%d\n", params_c);
-    printf("%d\n", params_i);
-    printf("%d\n", params_u);
-
-    // Read file
-    FILE * fp;
-    char * line = NULL;
-    size_t len = 0;
-    ssize_t read;
-    int index = 0;
-    fp  = fopen(argv[argc-1], "r");
-
-    while ((read = getline(&line, &len, fp)) != -1)
-    {
-        strcpy(list[index], line);
-        // printf("%s", line);
-        index++;
+    else{
+        return strcmp(s1, s2);
     }
-    fclose(fp);
+}
 
 
-    // Print Line
 
-    char temp[100];
-    strcpy(temp, list[0]);
-    for(int i = 0;i < index; i++)
+int find_func(char **list, char * tar, int *count, int cur_index){
+    
+    // return 0 if it is not unique
+    // return 1 if it is unique
+
+    if (compare_func(list[cur_index - 1], tar) != 0)
     {
-        if(i ==0)
+
+        if (params_u == 1 && cur_index + 1 < tot_index  && compare_func(list[cur_index + 1], tar) == 0)
         {
-            printf("%s", list[i]);
+            return 0;
         }
         else
         {
-            if(strcmp(temp, list[i]) != 0)
+            return 1;
+        }
+    }
+    else
+    {
+        count[cur_index - 1] += count[cur_index];
+        return 0;
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    char *list[100];
+    int count[100];
+    int print[100];
+
+    // Read parameters
+    while ((opt = getopt(argc, argv, "iuc")) != -1)
+    {
+        switch (opt)
+        {
+        case 'i':
+            params_i = 1;
+            option_all = option_all ^ 4;
+            break;
+        case 'u':
+            params_u = 1;
+            option_all = option_all ^ 2;
+            break;
+        case 'c':
+            params_c = 1;
+            option_all = option_all ^ 1;
+            break;
+        case '?':
+            if (opt == 'i' || opt == 'u' || opt == 'c')
+            {
+                fprintf(stderr, "Option -%c requires an argument.\n", opt);
+            }
+            else
+            {
+                fprintf(stderr, "Unknown option -%c\n", opt);
+            }
+            return 1;
+        default:
+            abort();
+        }
+    }
+    
+    // printf("c: %d\n", params_c);
+    // printf("i: %d\n", params_i);
+    // printf("u: %d\n", params_u);
+
+    // Initialize
+    for(int i = 0;i < 100;i++){
+        list[i] = malloc(100 * sizeof(char));
+        count[i] = 1;
+        print[i] = 0;
+    }
+
+    // Read file
+    FILE * fp;
+    char line_buf[100];
+    int len = 100;
+    
+    fp  = fopen(argv[argc-1], "r");
+
+    while (fgets(line_buf, 100, fp))
+    {
+        strcpy(list[tot_index], line_buf);
+
+        // printf("%s", list[tot_index]);
+        tot_index++;
+    }
+    fclose(fp);
+
+    // (BUG)
+    // Add nextline in the last string
+    strcat(list[tot_index - 1], "\n");
+
+    // Unique Function
+    for (int i = tot_index - 1; i >= 0; i--)
+    {
+        if(i == 0)
+        {
+            print[i] = 1;
+        }
+        else
+        {
+
+            if (find_func(list, list[i], count, i))
+            {
+                print[i] = 1;
+            }
+        }
+
+    }
+
+    // Print Function
+    for(int i = 0;i < tot_index;i++){
+        
+        if(print[i] == 1){
+            if (params_c == 1)
+            {
+                printf("      %d %s", count[i], list[i]);
+            }
+            else
             {
                 printf("%s", list[i]);
             }
         }
-
-        strcpy(temp, list[i]);
+        
     }
 
-    printf("\n");
+
+    // printf("\n");
+
     return 0;
 }
