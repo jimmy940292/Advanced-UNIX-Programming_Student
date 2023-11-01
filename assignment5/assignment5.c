602e0a22daf3dfb6882636994e83fc7eb16368f7
jimmy940292 <jimmy30213@gmail.com>
2023-11-01 17:13:52 +0800

    assignment5 ver.1

--- assignment5/assignment5.c
+++ assignment5/assignment5.c
@@ -6,33 +6,26 @@ int* f1(int val){
 
     int num = 0;
     int*ptr = &num;
+    // printf("Value %d is at %p in function f1\n", *ptr, ptr);
 
     if(num == 0){
         /* text had val == 0, but val is not in scope */
         int val;
         val = 5;
         ptr = &val;
-        // printf("Value %d is at %p\n", *ptr, ptr);
+        // printf("Value %d is at %p in function f1\n", *ptr, ptr);
     }
     
 
     return ptr;
 }
 
-void f2(void){
-
-    int v1 = 0;
-    int v2 = 5;
-    int v3 = 10;
-}
 
 int main(void){
 
     int val = 5;
     int *ptr = f1(val);
     
-    f2();
-
     printf("Value %d is at %p\n", *ptr, ptr);
 
 
