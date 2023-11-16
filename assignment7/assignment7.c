7629378b72ddf7699efd87e1d1653d3a4736a5a6
jimmy940292 <jimmy30213@gmail.com>
2023-11-16 17:28:05 +0800

    add assignment 7

---
+++ assignment7/assignment7.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int main(void){
+
+    // Create pipe
+    int link[2];
+    if (pipe(link) == -1)
+    {
+        printf("Create Pipe fail\n");
+        exit(-1);
+    }
+
+    // Create Child Process 
+    pid_t p = fork();
+    if(p == 0){
+
+    // Create New Session
+    int sid = setsid();
+    printf("Sid: %d\n", sid);
+
+    // Run 'ps' command
+    char *arg[3] = {"ps","-opid,pgrp,tpgid", NULL};
+    // char *arg[2] = {"ps", NULL};
+    close(link[0]);
+    dup2(link[1], STDOUT_FILENO);
+    execvp("ps", arg);
+
+    }else{
+
+        // Wait for pip
+        sleep(1);
+        close(link[1]);
+        char buf[1024];
+        FILE * pipe_out;
+
+        // Print output from ps command
+        int nbytes = read(link[0], buf, 1024);
+        printf("%d\n", nbytes);
+        printf("%s\n", buf);
+        
+    }
+
+    return 0;
+}
