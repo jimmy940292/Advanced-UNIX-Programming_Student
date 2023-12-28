6182b54a32533ab8dfa42c37f420c41036116964
jimmy940292 <jimmy30213@gmail.com>
2023-12-28 19:45:18 +0800

    first version of finalterm

---
+++ finalterm/q3.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+#include <signal.h>
+
+
+void sig_handler(){
+    printf("Alarm!\n");
+}
+
+void setAlarm(int sec)
+{
+    /* do something here ... */
+
+    signal(SIGALRM, sig_handler);
+    alarm(sec);
+    
+    
+}
+void clearAlarm()
+{
+    /* do something here ... */
+    alarm(0);
+}
+int main(void)
+{
+    // ...
+    /* You should copy and paste these test cases to your implementation */
+    setAlarm(2); // set 2 sec alarm at 0s, will finish at 2s after execution
+    sleep(1);
+    setAlarm(6); // set 6 sec alarm at 1s, will finish at 7s after execution
+    sleep(1);
+    setAlarm(3); // set 3 sec alarm at 2s, will finish at 5s after execution
+    sleep(4);
+    clearAlarm(); // clear all alarms at 6s after execution
+    //....
+    
+
+
+    return 0;
+}
